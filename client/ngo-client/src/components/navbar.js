c1056794a1bdf7a844ddbc8472f0be0a056beb2c
rishabh <rishabh7890@gmail.com>
2020-02-04 11:33:04 -0500

    admin and user portal buttons

--- client/ngo-client/src/components/navbar.js
+++ client/ngo-client/src/components/navbar.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
-
-export default function MenuAppBar({users}) {
+export default function MenuAppBar() {
   const classes = useStyles();
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  const username="";
 
   const handleChange = event => {
     setAuth(event.target.checked);
@@ -34,7 +32,6 @@ export default function MenuAppBar({users}) {
 
   const handleMenu = event => {
     setAnchorEl(event.currentTarget);
-    
   };
 
   const handleClose = () => {
@@ -45,7 +42,6 @@ export default function MenuAppBar({users}) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          
           <Typography variant="h6" align="center" className={classes.title}>
             <h2>NGO-Donations</h2>
           </Typography>
@@ -59,22 +55,19 @@ export default function MenuAppBar({users}) {
                 color="inherit"
                 size="medium"
               >
-              {users[0].userFname}
-              <AccountCircle />
-                
-                
+                <AccountCircle />
               </IconButton>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right"
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right"
                 }}
                 open={open}
                 onClose={handleClose}
